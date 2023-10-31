a1f1c006bda23888fee68b7bd30d215af1c3332c
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-31 13:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+ const itemsContainer = document.querySelector(".items");
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
+
+    itemsContainer.addEventListener("mousedown", (e) => {
+      isDragging = true;
+      startX = e.pageX - itemsContainer.offsetLeft;
+      scrollLeft = itemsContainer.scrollLeft;
+    });
+
+    itemsContainer.addEventListener("mouseup", () => {
+      isDragging = false;
+    });
+
+    itemsContainer.addEventListener("mouseleave", () => {
+      isDragging = false;
+    });
+
+    itemsContainer.addEventListener("mousemove", (e) => {
+      if (!isDragging) return;
+      e.preventDefault();
+      const x = e.pageX - itemsContainer.offsetLeft;
+      const walk = (x - startX) * 3; // You can adjust the multiplier to control the speed
+      itemsContainer.scrollLeft = scrollLeft - walk;
+    });
